6039dae59fbcbf9788a75088a6efdaed0776042e
KUNE23 <nuansajunti8@gmail.com>
2026-01-14 08:13:42 +0700

    update logout function & icon logout in DashboardActivity.java

--- app/src/main/java/com/example/dompetku/DashboardActivity.java
+++ app/src/main/java/com/example/dompetku/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.example.dompetku;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton; // Import baru
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ public class DashboardActivity extends AppCompatActivity {
     DatabaseHelper db;
     TextView tvAvatar, tvUserName, tvUserEmail, tvSaldoDisplay;
     Button btnPemasukan, btnPengeluaran, btnRiwayat;
+    ImageButton btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class DashboardActivity extends AppCompatActivity {
         btnPemasukan = findViewById(R.id.btnPemasukan);
         btnPengeluaran = findViewById(R.id.btnPengeluaran);
         btnRiwayat = findViewById(R.id.btnRiwayat);
+        btnLogout = findViewById(R.id.btnLogout);
 
         userName = getIntent().getStringExtra("USER_NAME");
         userEmail = getIntent().getStringExtra("USER_EMAIL");
@@ -38,6 +41,15 @@ public class DashboardActivity extends AppCompatActivity {
             String initial = String.valueOf(userName.charAt(0)).toUpperCase();
             tvAvatar.setText(initial);
         }
+
+        btnLogout.setOnClickListener(v -> {
+            Toast.makeText(DashboardActivity.this, "Berhasil Logout", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(DashboardActivity.this, SignInActivity.class);
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        });
 
         btnPemasukan.setOnClickListener(v -> {
             Intent intent = new Intent(DashboardActivity.this, PemasukanActivity.class);
